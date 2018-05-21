7fab162610f37d541889d2978d48c8919527d505
DanSoup <dansoup@hotmail.co.uk>
2018-05-21 19:23:06 +0100

    downloading all info from db

--- src/components/API.js
+++ src/components/API.js
@@ -4,11 +4,37 @@ const domain = 'https://flashcube-back-end.herokuapp.com'
 
 export function getUserById (userId) {
 
+    console.log(userId)
+
+    console.log(`${domain}/api/users/${userId}`)
+
     return axios
         .get(`${domain}/api/users/${userId}`, )
         .then(res => {
             console.log(res);
-            return res;
+            return res.data.result.records;
+        })
+        .catch(console.log);
+
+}
+
+export function getAllTopics () {
+
+    return axios
+        .get(`${domain}/api/topics`, )
+        .then(res => {
+            return res.data.result.records;
+        })
+        .catch(console.log);
+
+}
+
+export function getAllTerms () {
+
+    return axios
+        .get(`${domain}/api/terms`, )
+        .then(res => {
+            return res.data.result.records;
         })
         .catch(console.log);
 
